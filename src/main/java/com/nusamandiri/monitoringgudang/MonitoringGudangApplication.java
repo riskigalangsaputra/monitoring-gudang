d3fb0fc7313106a796e5e1d5f396becda624b7ac
riskigalang <riskigalangsaputra@gmail.com>
2022-05-25 10:06:14 +0700

    setup project

---
+++ src/main/java/com/nusamandiri/monitoringgudang/MonitoringGudangApplication.java
@@ -0,0 +1,13 @@
+package com.nusamandiri.monitoringgudang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringGudangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringGudangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nusamandiri/monitoringgudang/MonitoringGudangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nusamandiri.monitoringgudang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringGudangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
