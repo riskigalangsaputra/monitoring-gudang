069bb8b0ac20a5c6240385229f366de71ed4c676
riskigalang <riskigalangsaputra@gmail.com>
2022-05-30 10:37:17 +0700

    push 2

--- src/test/java/com/nusamandiri/monitoringgudang/MonitoringGudangApplicationTests.java
+++ src/test/java/com/nusamandiri/monitoringgudang/MonitoringGudangApplicationTests.java
@@ -3,11 +3,21 @@ package com.nusamandiri.monitoringgudang;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 class MonitoringGudangApplicationTests {
 
 	@Test
 	void contextLoads() {
+		for (int i=1; i <=5; i++) {
+			if (i % 2 == 0) {
+				System.out.println("");
+			} else {
+
+			}
+		}
 	}
 
 }
